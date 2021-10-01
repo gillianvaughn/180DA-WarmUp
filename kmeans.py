f64a379f94ab17d5bef88dd5d6f8b30c00011aae
gillianvaughn <gillymvaughn@gmail.com>
2021-10-01 16:56:46 -0700

    opencv

---
+++ kmeans.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+cap = cv2.VideoCapture(0)
+
+def find_histogram(clt):
+    """
+    create a histogram with k clusters
+    :param: clt
+    :return:hist
+    """
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+while True:
+    _, frame = cap.read()
+
+    lower_blue = np.array([100,0,0])  
+    upper_blue = np.array([255,100,100])
+     # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange (frame, lower_blue, upper_blue)
+    bluecnts = cv2.findContours(mask.copy(),
+                              cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    if len(bluecnts)>0:
+        blue_area = max(bluecnts, key=cv2.contourArea)
+        (xg,yg,wg,hg) = cv2.boundingRect(blue_area)
+        cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+        box_img = frame[yg:yg+hg,xg:xg+wg]
+
+        cv2.imshow('frame',frame)
+
+        #selects a random box as the image and reshapes it like 
+        box_img=cv2.cvtColor(box_img,cv2.COLOR_BGR2RGB)
+        box_img = box_img.reshape((box_img.shape[0] * box_img.shape[1],3))
+
+        clt = KMeans(n_clusters=3) #cluster number
+        clt.fit(box_img)
+
+        hist = find_histogram(clt)
+        bar = plot_colors2(hist, clt.cluster_centers_)
+
+        plt.axis("off")
+        plt.imshow(bar)
+        plt.show()
+        
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+"""
+References:
+Dominant Color: https://code.likeagirl.io/finding-dominant-colour-on-an-image-b4e075f98097
+Bounding Box: https://answers.opencv.org/question/200861/drawing-a-rectangle-around-a-color-as-shown/
+"""
